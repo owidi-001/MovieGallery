45aeafa84150a39aab5d8a39ca21b618bda5680e
owidi-001 <kevinalex846@gmail.com>
2022-02-28 22:10:27 +0300

    basic conf,models,templates

---
+++ Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }=String.Empty;
+        public string Description { get; set; }=String.Empty;
+        
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }=String.Empty;
+        public decimal Price { get; set; }
+
+
+    }
+}
---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MoviesApp.Data;
+using MoviesApp.Models;
+
+namespace MoviesApp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MoviesApp.Data.MoviesAppContext _context;
+
+        public IndexModel(MoviesApp.Data.MoviesAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
